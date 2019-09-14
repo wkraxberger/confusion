c3afc4d9598e3eaed9b4bfa5aab9432e0d91281c
Walter Kraxberger <wkrax@hotmail.com>
2019-09-14 16:05:58 -0300

    changed components to be functional

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
 
-class DishDetail extends Component {
 
-	constructor(props) {
-		super(props);
-	}
-
-	renderDish(dish) {
+	function RenderDish({dish}) {
 		return(
 			<div className="col-12 col-md-5 m-1">
 				<Card>
@@ -22,7 +17,7 @@ class DishDetail extends Component {
 		
 	}
 
-	renderComments(comments)
+	function RenderComments({comments})
 	{
 		const content = comments.map((comment) => {
 			return(
@@ -41,13 +36,13 @@ class DishDetail extends Component {
 		);
 	}
 
-	render() {
-		if (this.props.dish != null) {
+	const DishDetail = (props) => {
+		if (props.dish != null) {
 			return(
 				<div class="container">
 					<div className="row">
-						{this.renderDish(this.props.dish)}
-						{this.renderComments(this.props.dish.comments)}
+						<RenderDish dish={props.dish} />
+						<RenderComments comments={props.dish.comments} />
 					</div>
 				</div>
 			);
@@ -59,6 +54,6 @@ class DishDetail extends Component {
 			);
 		}
 	}
-}
+
 
 export default DishDetail;
