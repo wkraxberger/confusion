f5306b61326546843e717c3378c00b41da84132f
Walter Kraxberger <wkrax@hotmail.com>
2019-09-23 11:42:01 -0300

    Added breadcrumbs. enhanced view

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, 
+		CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 	function RenderDish({dish}) {
 		return(
@@ -41,8 +42,18 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 			return(
 				<div class="container">
 					<div className="row">
+						<Breadcrumb>
+							<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>						
+							<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+						</Breadcrumb>
+					</div>
+					<div className="col-12">
+						<h3>Menu</h3>
+						<hr />
+					</div>
+					<div className="row">
 						<RenderDish dish={props.dish} />
-						<RenderComments comments={props.dish.comments} />
+						<RenderComments comments={props.comments} />
 					</div>
 				</div>
 			);
