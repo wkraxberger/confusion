b61c104445c47999608469c4f3f3ff22f0452248
Walter Kraxberger <wkrax@hotmail.com>
2019-10-02 23:59:44 -0300

    Added comment submit button

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -47,35 +47,35 @@ class Header extends Component {
           		    			alt="Ristorante Con Fusion" />
           		    	</NavbarBrand>
           		    	<Collapse isOpen={this.state.isNavOpen} navbar>
-          		    	<Nav navbar>
-          		    		<NavItem>
-          		    			<NavLink className="nav-link" to="/home" >
-          		    				<span className="fa fa-home fa-lg"></span> Home
-          		    			</NavLink>
-          		    		</NavItem>
-          		    		<NavItem>
-          		    			<NavLink className="nav-link" to="/aboutus" >
-          		    				<span className="fa fa-info fa-lg"></span> About Us
-          		    			</NavLink>
-          		    		</NavItem>
-          		    		<NavItem>
-          		    			<NavLink className="nav-link" to="/menu" >
-          		    				<span className="fa fa-list fa-lg"></span> Menu
-          		    			</NavLink>
-          		    		</NavItem>
-          		    		<NavItem>
-          		    			<NavLink className="nav-link" to="/contactus" >
-          		    				<span className="fa fa-address-card fa-lg"></span> Contact Us
-          		    			</NavLink>
-          		    		</NavItem>
-          		    	</Nav>	
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <Button outline onClick={this.toggleModal}>
-                                <span classNam="fa fa-sign-in fa-lg"></span> Login
-                            </Button>
-                        </NavItem>
-                    </Nav>
+            		    	<Nav navbar>
+            		    		<NavItem>
+            		    			<NavLink className="nav-link" to="/home" >
+            		    				<span className="fa fa-home fa-lg"></span> Home
+            		    			</NavLink>
+            		    		</NavItem>
+            		    		<NavItem>
+            		    			<NavLink className="nav-link" to="/aboutus" >
+            		    				<span className="fa fa-info fa-lg"></span> About Us
+            		    			</NavLink>
+            		    		</NavItem>
+            		    		<NavItem>
+            		    			<NavLink className="nav-link" to="/menu" >
+            		    				<span className="fa fa-list fa-lg"></span> Menu
+            		    			</NavLink>
+            		    		</NavItem>
+            		    		<NavItem>
+            		    			<NavLink className="nav-link" to="/contactus" >
+            		    				<span className="fa fa-address-card fa-lg"></span> Contact Us
+            		    			</NavLink>
+            		    		</NavItem>
+            		    	</Nav>	
+                      <Nav className="ml-auto" navbar>
+                          <NavItem>
+                              <Button outline onClick={this.toggleModal}>
+                                  <span className="fa fa-sign-in fa-lg"></span> Login
+                              </Button>
+                          </NavItem>
+                      </Nav>
           		    	</Collapse>
           	    	</div>
       	    	</Navbar>
