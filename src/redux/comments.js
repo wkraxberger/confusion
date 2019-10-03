59b798669cefa87198fbfe370a318f790dfc3e9f
Walter Kraxberger <wkrax@hotmail.com>
2019-10-03 00:10:41 -0300

    fixed reducers

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,6 +1,6 @@
 import { COMMENTS } from '../shared/comments';
 
-export const Dishes = (state = COMMENTS, action) => {
+export const Comments = (state = COMMENTS, action) => {
 	switch(action.type) {
 		default:
 			return state;
