c6887678a0f800769d90d48225fdb31c4fadc27f
Walter Kraxberger <wkrax@hotmail.com>
2019-10-04 18:30:18 -0300

    Moved images and data to web service

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,13 +1,13 @@
 import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl';
 
 export const fetchDishes = () => (dispatch) => {
-
 	dispatch(dishesLoading(true));
 
-	setTimeout(() => {
-		dispatch(addDishes(DISHES));
-	}, 2000);
+	return fetch(baseUrl + 'dishes')
+		.then(response => response.json())
+		.then(dishes => dispatch(addDishes(dishes)));
 }
 
 export const dishesLoading = () => ({
@@ -22,7 +22,7 @@ export const dishesFailed = (errMess) => ({
 export const addDishes = (dishes) => ({
 	type: ActionTypes.ADD_DISHES,
 	payload: dishes
-})
+});
 
 export const addComment = (dishId, rating, author, comment) => ({
 	type: ActionTypes.ADD_COMMENT,
@@ -32,4 +32,43 @@ export const addComment = (dishId, rating, author, comment) => ({
 		author: author,
 		comment: comment
 	}
+});
+
+export const fetchComments = () => (dispatch) => {
+	return fetch(baseUrl + 'comments')
+		.then(response => response.json())
+		.then(comments => dispatch(addComments(comments)));
+}
+
+export const commentsFailed = (errMess) => ({
+	type: ActionTypes.COMMENTS_FAILED,
+	payload: errMess
+});
+
+export const addComments = (comments) => ({
+	type: ActionTypes.ADD_COMMENTS,
+	payload: comments
+});
+
+
+export const fetchPromos = () => (dispatch) => {
+	dispatch(promosLoading(true));
+
+	return fetch(baseUrl + 'promotions')
+		.then(response => response.json())
+		.then(promos => dispatch(addPromos(promos)));
+}
+
+export const promosLoading = () => ({
+	type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errMess) => ({
+	type: ActionTypes.PROMOS_FAILED,
+	payload: errMess
+});
+
+export const addPromos = (promos) => ({
+	type: ActionTypes.ADD_PROMOS,
+	payload: promos
 });
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -8,12 +8,15 @@ export const Dishes = (state = { isLoading: true,
 			case ActionTypes.ADD_DISHES:
 				return {...state, isLoading: false, errMess: null, 
 						dishes: action.payload };
+						
 			case ActionTypes.DISHES_LOADING:
 				return {...state, isLoading: true, errMess: null,
 						dishes: [] };
+
 			case ActionTypes.DISHES_FAILED:
 				return {...state, isLoading: false, 
 						errMess: action.payload };
+
 			default:
 				return state;
 	}
